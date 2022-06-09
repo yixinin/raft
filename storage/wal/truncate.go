eab27b5f70ea875043df8439b3b4632c98392b4f
eason <yixinin@outlook.com>
2022-06-09 20:10:04 +0800

    fix

--- storage/wal/truncate.go
+++ storage/wal/truncate.go
@@ -12,6 +12,6 @@ func (w *recordWriter) Truncate(offset int64) error {
 		return err
 	}
 	w.offset = offset
-	_, err = w.f.Seek(offset, os.SEEK_SET)
+	_, err := w.f.Seek(offset, os.SEEK_SET)
 	return err
 }
